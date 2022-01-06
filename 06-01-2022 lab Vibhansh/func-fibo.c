97dfbbfafad7a49f68335eaddae34274ab7aa626
Vibhansh Agrawal <91672234+vibhanshag@users.noreply.github.com>
2022-01-06 13:21:07 +0200

    Add files via upload

---
+++ 06-01-2022 lab Vibhansh/func-fibo.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+void fibo(int nn)
+{
+	int a=0,b=1,c;
+	while(a<=nn)
+	{
+		c=a+b;
+		printf("%d\t",a);
+		a=b;
+		b=c;
+	}
+}
+int main()
+{
+	int n;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
---
+++ 06-01-2022 lab Vibhansh/str-pattrn.c
@@ -0,0 +1,14 @@
+//string pattern printing
+int main()
+{
+	int count=0,i;
+	char x[10];
+	gets(x);
+	for ()
+	/*for(i=0;x[i]!='\0';i++)
+	{
+		count++;
+	}
+	printf("%d",count);*/
+	return 0;
+}
